94ed3db61a3c5acb9936df34d13b5695a1c47ea1
Kosiarznerek <dbabka.poczta@gmail.com>
2019-10-20 12:58:52 +0200

    Import portfolio 2.0

--- projects/14/scripts/SOUNDS.js
+++
@@ -1,4 +0,0 @@
-﻿const SOUNDS = {
-    shoot: new Audio("sounds/shoot.wav"),
-    destroyed: new Audio("sounds/destroyed.mp3"),
-}
--- projects/15/biblioteki/Sleep.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Sleep in async function
- * @param {number} ms
- * @returns {Promise<boolean>}
- */
-const Sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(true), ms));
